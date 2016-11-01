8c9124aa2522179f2865e26add05926ca017d824
jessicalettes <jessicalettes@gmail.com>
2016-11-01 15:41:48 -0700

    Add Emotion Bar Chart

---
+++ src/components/EmotionBarChart.js
@@ -0,0 +1,37 @@
+import {Chart} from 'react-google-charts';
+import React from 'react';
+
+class EmotionBarChart extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state={
+			options:{
+				title: 'Happy',
+				hAxis: {title: 'Event'},
+				vAxis: {title: '% Time Happy'},
+				legend: 'none',
+			},
+			data:[
+				['Element','Happiness'],
+				['School',75],
+				['Work',50],
+				['Club',100],
+			]
+		};
+	}
+	render() {
+		return (
+			<Chart
+			chartType="ColumnChart"
+			//eventName="onmouseover"
+			data={this.state.data}
+			options={this.state.options}
+			graph_id="EmotionBarChart"
+			width={window.innerWidth/2}
+			height={window.innerWidth/2}
+			/>
+		);
+	}
+};
+
+export default EmotionBarChart;
